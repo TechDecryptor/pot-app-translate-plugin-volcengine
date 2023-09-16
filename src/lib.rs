01c8607f3acf5cb372ac3bfb26baccf0b08fb23f
TechDecryptor <145176897+TechDecryptor@users.noreply.github.com>
2023-09-16 03:17:21 +0000

    Init

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,35 @@
 use serde_json::Value;
 use std::collections::HashMap;
 use std::error::Error;
-use urlencoding::encode;
 
 #[no_mangle]
 pub fn translate(
-    text: &str, // 待翻译文本
-    from: &str, // 源语言
-    to: &str,   // 目标语言
-    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
-    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+    text: &str,
+    from: &str,
+    to: &str,
+    _needs: HashMap<String, String>,
 ) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::blocking::ClientBuilder::new().build()?;
 
-    let mut url = match needs.get("requestPath") {
-        Some(url) => url.to_string(),
-        None => return Err("requestPath not found".into()),
-    };
+    const URL: &str = "https://translate.volcengine.com/crx/translate/v1";
 
-    if !url.starts_with("http") {
-        url = format!("https://{}", url);
-    }
-
-    let plain_text = text.replace("/", "@@");
-    let encode_text = encode(plain_text.as_str());
+    let mut body = HashMap::new();
+    body.insert("source_language", from);
+    body.insert("target_language", to);
+    body.insert("text", text);
 
     let res: Value = client
-        .get(format!("{url}/api/v1/{from}/{to}/{encode_text}"))
+        .post(URL)
+        .header("Content-Type", "application/json")
+        .json(&body)
         .send()?
         .json()?;
 
     fn parse_result(res: Value) -> Option<String> {
+        println!("{res:?}");
         let result = res.as_object()?.get("translation")?.as_str()?.to_string();
 
-        Some(result.replace("@@", "/"))
+        Some(result)
     }
     if let Some(result) = parse_result(res) {
         return Ok(Value::String(result));
@@ -47,9 +43,8 @@ mod tests {
     use super::*;
     #[test]
     fn try_request() {
-        let mut needs = HashMap::new();
-        needs.insert("requestPath".to_string(), "lingva.pot-app.com".to_string());
-        let result = translate("你好 世界！", "auto", "en", needs).unwrap();
+        let needs = HashMap::new();
+        let result = translate("你好", "detect", "en", needs).unwrap();
         println!("{result}");
     }
 }
